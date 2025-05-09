cbc9c5d2d10abe95aec845aff9d96667f7606c82
Rehan <rehantechnical19@gmail.com>
2025-05-09 14:11:54 +0530

    roll back

--- backend/index.js
+++ backend/index.js
@@ -31,7 +31,6 @@ app.use(cors(corsOptions));
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/post", postRoute);
 app.use("/api/v1/message", messageRoute);
-app.use("/api/v1/auth", authRoutes);
 
 
 // app.use(express.static(path.join(__dirname, "/frontend/dist")));
