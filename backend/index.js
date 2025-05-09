7dafe75363dd13d862afb729d71ae1c72e106d0a
Rehan <rehantechnical19@gmail.com>
2025-05-09 14:11:01 +0530

    fix 2

--- backend/index.js
+++ backend/index.js
@@ -31,6 +31,7 @@ app.use(cors(corsOptions));
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/post", postRoute);
 app.use("/api/v1/message", messageRoute);
+app.use("/api/v1/auth", authRoutes);
 
 
 // app.use(express.static(path.join(__dirname, "/frontend/dist")));
